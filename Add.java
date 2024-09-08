c63db4285ad2d8fb6d9f89b892eb4946de8b5d50
imrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-09-08 18:28:24 +0530

    new java file ADDED

---
+++ Add.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Add{
+
+
+    public static void main(String[] args){
+        System.out.println("Enter first Number");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+ System.out.println("Enter Second Number");
+ int b = sc.nextInt();
+
+ int c = a + b;
+ System.out.println("The Sum is "+c);
+ System.out.println("------------------Thanks for Using --------------------------");
+
+    }
+}
